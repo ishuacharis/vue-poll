e7481be2f7cac4865613a326d21e2c8c55582e3e
ishuacharis <ishuacharis@gmail.com>
2021-03-08 02:01:49 +0100

    display notification

--- src/components/Notification/Notification.vue
+++ src/components/Notification/Notification.vue
@@ -1,16 +1,24 @@
 <template  lang="html">
-    <div class="notification">
-        Notifications
+    <div class="notification" >
+        {{ notificationType }}
     </div>
 </template>
 
 <script>
-
+    import { ref } from 'vue';
     export default {
         name: 'Notification',
-        
-        setup() {
-
+        props: {
+            notification: Object
+        },
+        setup(props) {
+            const notificationType = ref(props.notification.type)
+            if (notificationType.value == 'UserLoginNotification') {
+                notificationType.value = "You have just logged in"
+            }
+            return{
+                notificationType
+            }
         }
     }
 </script>
