dc94b262fd50737985815d5ac66927821b7196f4
ishuacharis <ishuacharis@gmail.com>
2021-03-09 02:52:18 +0100

    destructure props

--- src/components/Notification/Notification.vue
+++ src/components/Notification/Notification.vue
@@ -1,23 +1,24 @@
 <template  lang="html">
     <div class="notification" >
-        {{ notificationType }}
+        {{ type }}
     </div>
 </template>
 
 <script>
-    import { ref } from 'vue';
+    import { toRefs } from 'vue';
     export default {
         name: 'Notification',
         props: {
             notification: Object
         },
         setup(props) {
-            const notificationType = ref(props.notification.type)
-            if (notificationType.value == 'UserLoginNotification') {
-                notificationType.value = "You have just logged in"
+            const { notification } = toRefs(props)
+            let { type } = notification.value
+            if (type == 'UserLoginNotification') {
+                type = "You have just logged in"
             }
             return{
-                notificationType
+                type
             }
         }
     }
