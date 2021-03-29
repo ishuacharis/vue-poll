2c9c59623e679e0daec1f3b8b3894e613d461e0d
ishuacharis <ishuacharis@gmail.com>
2021-03-29 10:37:56 +0100

    add isloading and scroll feature

--- src/views/Notifications/Notifications.vue
+++ src/views/Notifications/Notifications.vue
@@ -1,17 +1,25 @@
 <template lang="html">
     
     <div class="notifications" v-if="notificationsObject">
-        <Notification v-for="notification in notificationsObject.data"  
-        :key="notification.id"
-        :notification ="notification" 
-        />
+        <template v-if="isLoading">
+            <div class="loading-container">
+                <div class="loading"></div>
+            </div>
+        </template>
+        <template v-if="!isLoading">
+
+            <Notification v-for="notification in notificationsObject.data"  
+            :key="notification.id"
+            :notification ="notification" 
+            />
+        </template>
     </div>
 
 </template>
 
 <script>
     import { pusherClient } from '@/Context/PusherContext';
-    import { onMounted, reactive} from 'vue';
+    import { onMounted, reactive, ref} from 'vue';
     import routes from '@/routes';
     import { getUserId } from '@/helpers';
     import Notification from '@/components/Notification/Notification';
@@ -23,7 +31,9 @@
         setup() {
             const { notifications } = routes();
             const notificationsObject = reactive({});
+            const isLoading = ref(true);
             const userId =  getUserId();
+            const count  = ref(10)
             let args = {
                 endPoint: `/notifications/${userId}`,
                 method: 'GET',
@@ -32,15 +42,30 @@
                 const { response:{ message:m, notifications:{ data} } } = await notifications(args);
                 notificationsObject.message = m
                 notificationsObject.data = data
+                isLoading.value = false
             }
-            onMounted(userNotifications)
+            onMounted(() => {
+                userNotifications()
+                scroll()
+            })
             const messageChannel = pusherClient.subscribe('login-channel');
             messageChannel.bind('login-event', function(data) {
                 console.log(`notifications ${data}`);
             });
+            const scroll = () => {
+                window.onscroll = () => {
+                    let bottomOfWindow = document.documentElement.scrollTop + window.innerHeight === document.documentElement.offsetHeight;
+
+                    if(bottomOfWindow) {
+                        
+                        count.value += 5
+                        console.log(count.value)
+                    }
+                }
+            }
             return  {
                 
-                userNotifications,notificationsObject
+                userNotifications,notificationsObject, isLoading
             }
         }
         
