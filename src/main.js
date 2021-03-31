78175bef8d284b2fc7dcd3eee2b3b0a17e20fdad
ishuacharis <ishuacharis@gmail.com>
2021-03-31 16:57:54 +0100

    remove directive

--- src/main.js
+++ src/main.js
@@ -2,17 +2,16 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Greeting  from './plugins/hello_plugin'
 
 const app = createApp(App)
-
-app
-.directive('color', {
-  mounted(el, binding) {
-    const color  = binding.value || 'gray'
-    console.log(color)
-    el.style.backgroundColor = color
+const options = {
+  greetings: {
+    hi: "Sola"
   }
-})
+}
+app
+.use(Greeting, options)
 .use(router)
 .use(store)
 .mount('#app')
