729d1ac45a845746558d3151b618980c1faafead
olawale-o <omoogunolawale@gmail.com>
2021-04-21 02:59:57 +0100

    fix exports and include form values for schema

--- src/schema/index.js
+++ src/schema/index.js
@@ -1,51 +1,51 @@
 import * as Yup from 'yup'
-export default function() {
-    const registerSchema = Yup.object().shape({
-        name: Yup.string()
-        .required('Name is required'),
-        email: Yup.string()
-        .email("Email is invalid")
-        .required('Email is required'),
-        phoneNo: Yup.string()
-        .required('Phone number is required'),
-        password: Yup.string()
-        .required("Password is required")
-        .min(4, "Password should be atleast 4 characters")
-        .max(10, "Password should be not be more than 10 characters")
-    })
 
-    const loginSchema = Yup.object().shape({
-        email: Yup.string()
-        .email("Email is invalid")
-        .required('Email is required'),
-        username: Yup.string()
-        .required('Username is required'),
-        password: Yup.string()
-        .required("Password is required")
-        .min(4, "Password should be atleast 4 characters")
-        .max(10, "Password should be not be more than 10 characters")
-    })
+export const registerSchema = Yup.object().shape({
+    name: Yup.string()
+    .required('Name is required'),
+    email: Yup.string()
+    .email("Email is invalid")
+    .required('Email is required'),
+    phoneNo: Yup.string()
+    .required('Phone number is required'),
+    password: Yup.string()
+    .required("Password is required")
+    .min(4, "Password should be atleast 4 characters")
+    .max(10, "Password should be not be more than 10 characters")
+})
 
-    const forgotPasswordSchema = Yup.object().shape({
-        email: Yup.string()
-        .email("Email is invalid")
-        .required('Email is required'),
-    })
+export const loginSchema = Yup.object().shape({
+    email: Yup.string()
+    .email("Email is invalid")
+    .required('Email is required'),
+    username: Yup.string()
+    .required('Username is required'),
+    password: Yup.string()
+    .required("Password is required")
+    .min(4, "Password should be atleast 4 characters")
+    .max(10, "Password should be not be more than 10 characters")
+})
 
-    const resetPasswordSchema = Yup.object().shape({
-        token: Yup.string()
-        .required('Token is required'),
-        email: Yup.string()
-        .email("Email is invalid")
-        .required('Email is required'),
-        password: Yup.string()
-        .required("Password is required")
-        .min(4, "Password should be atleast 4 characters")
-        .max(10, "Password should be not be more than 10 characters")
-    })
+export const forgotPasswordSchema = Yup.object().shape({
+    email: Yup.string()
+    .email("Email is invalid")
+    .required('Email is required'),
+})
+
+export const resetPasswordSchema = Yup.object().shape({
+    token: Yup.string()
+    .required('Token is required'),
+    email: Yup.string()
+    .email("Email is invalid")
+    .required('Email is required'),
+    password: Yup.string()
+    .required("Password is required")
+    .min(4, "Password should be atleast 4 characters")
+    .max(10, "Password should be not be more than 10 characters")
+});
 
 
-    return {
-        loginSchema, registerSchema, forgotPasswordSchema, resetPasswordSchema
-    }
-}
+export const registerFormValues = { name: '', email: '', phoneNo: '', password: '' };
+export const loginFormValues =  { email: '', password: '' };
+export const resetPasswordFormValues = { token: '', email: '',  password: ''};
+export const  forgotPasswordFormValues = { email: '',  };
