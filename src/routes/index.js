af244db3fc08af7ef1784514de45a1307193ce0b
olawale-o <omoogunolawale@gmail.com>
2021-07-19 17:50:27 +0100

    Modify index.js file

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,98 +1,92 @@
-  
-    const BASE_URI = "http://localhost:8000/api";
-    const headers = {
-        headers: {
-            "Content-type": "application/json;charset=UTF-8",
-            "Accept": "application/json"
-        }
-    };
+const BASE_URI = "http://localhost:8000/api";
+const headers = {
+  headers: {
+    "Content-type": "application/json;charset=UTF-8",
+    "Accept": "application/json"
+  }
+};
 
-    const connect = async (args) => {
-      
-        const endPoint  = args["endPoint"];
-        const body =  args["body"];
-        const method = args["method"];
-        const token  = args['token'] || '';
-        const authorization = token ?  `Bearer ${token}` : '' ;
-        headers["headers"]["Authorization"] = authorization;
-        const uri = BASE_URI+endPoint; 
-        const options = {
-            method: method,
-            headers: headers["headers"],
-            body: JSON.stringify(body)
-        }
-        const response = await fetch(uri,options)
-        if ( !response.ok ) {
-            const { response: { message } } = await response.json();
-            
-            throw new Error(message);
-        }
+const connect = async (args) => {
+  const endPoint = args["endPoint"];
+  const body = args["body"];
+  const method = args["method"];
+  const token  = args['token'] || '';
+  const authorization = token ?  `Bearer ${token}` : '' ;
+  headers["headers"]["Authorization"] = authorization;
+  const uri = BASE_URI+endPoint; 
+  const options = {
+    method: method,
+    headers: headers["headers"],
+    body: JSON.stringify(body)
+  }
+  const response = await fetch(uri, options)
+  if (!response.ok) {
+    const { response: { message } } = await response.json();
+    throw new Error(message);
+  }
+  return await response.json();
+};
 
-        return await response.json();
-    };
-
-export  const login = async (args) => {
-        return await connect(args);
-    };
+export const login = async (args) => {
+  return await connect(args);
+};
 
 export  const register = async (args) => {
-        return await connect(args);
-    };
+  return await connect(args);
+};
 
 export const forgotPassword = async (args) => {
-        return await connect(args);
-       
-    };
+  return await connect(args);     
+};
 
 export const resetPassword = async (args) => {
-        return await connect(args);
-    };
+  return await connect(args);
+};
 
 export const logout = async (args) =>  {
-        return await connect(args);
-    };
+  return await connect(args);
+};
     
 export const notifications =  async (args) => {
-        const endPoint  = args["endPoint"];
-        const method = args["method"];
-        const token  = args['token'] || '';
-        const authorization = token ?  `Bearer ${token}` : '' ;
-        headers["headers"]["Authorization"] = authorization;
-        const uri = BASE_URI+endPoint; 
-        const options = {
-            method: method,
-            headers: headers["headers"],
-        }
-        const response = await fetch(uri,options)
-        if ( !response.ok ) {
-            const { response: { message } } = await response.json();
-            
-            throw new Error(message);
-        }
+  const endPoint  = args["endPoint"];
+  const method = args["method"];
+  const token  = args['token'] || '';
+  const authorization = token ?  `Bearer ${token}` : '' ;
+  headers["headers"]["Authorization"] = authorization;
+  const uri = BASE_URI+endPoint; 
+  const options = {
+    method: method,
+    headers: headers["headers"],
+  }
 
-        return await response.json();
-    };
+ const response = await fetch(uri,options)
+ if (!response.ok) {
+  const { response: { message } } = await response.json();
+  throw new Error(message);
+ }
+
+ return await response.json();
+};
 
 export const vote = async (args) => {
-        return await connect(args);
-    };
+  return await connect(args);
+};
 
 export const eviction = async (args) => {
-        const endPoint  = args["endPoint"];
-        const method = args["method"];
-        const token  = args['token'] || '';
-        const authorization = token ?  `Bearer ${token}` : '' ;
-        headers["headers"]["Authorization"] = authorization;
-        const uri = BASE_URI+endPoint; 
-        const options = {
-            method: method,
-            headers: headers["headers"],
-        }
-        const response = await fetch(uri,options)
-        if ( !response.ok ) {
-            const { response: { message} } = await response.json();           
-            throw new Error(message);
-        }
-
-        return await response.json();
-    };
+  const endPoint  = args["endPoint"];
+  const method = args["method"];
+  const token  = args['token'] || '';
+  const authorization = token ?  `Bearer ${token}` : '' ;
+  headers["headers"]["Authorization"] = authorization;
+  const uri = BASE_URI+endPoint; 
+  const options = {
+    method: method,
+    headers: headers["headers"],
+  }
+  const response = await fetch(uri,options)
+  if (!response.ok) {
+    const { response: { message} } = await response.json();           
+    throw new Error(message);
+  }
+  return await response.json();
+};
