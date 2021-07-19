3c09e7dc6f2685b8d1265b1aab82a92d60cc5826
olawale-o <omoogunolawale@gmail.com>
2021-07-19 17:57:56 +0100

    Modify index.js

--- src/schema/index.js
+++ src/schema/index.js
@@ -1,51 +1,50 @@
-import * as Yup from 'yup'
+import * as Yup from 'yup';
 
 export const registerSchema = Yup.object().shape({
-    name: Yup.string()
-    .required('Name is required'),
-    email: Yup.string()
-    .email("Email is invalid")
-    .required('Email is required'),
-    phoneNo: Yup.string()
-    .required('Phone number is required'),
-    password: Yup.string()
-    .required("Password is required")
-    .min(4, "Password should be atleast 4 characters")
-    .max(10, "Password should be not be more than 10 characters")
-})
-
-export const loginSchema = Yup.object().shape({
-    email: Yup.string()
-    .email("Email is invalid")
-    .required('Email is required'),
-    username: Yup.string()
-    .required('Username is required'),
-    password: Yup.string()
-    .required("Password is required")
-    .min(4, "Password should be atleast 4 characters")
-    .max(10, "Password should be not be more than 10 characters")
-})
-
-export const forgotPasswordSchema = Yup.object().shape({
-    email: Yup.string()
-    .email("Email is invalid")
-    .required('Email is required'),
-})
-
-export const resetPasswordSchema = Yup.object().shape({
-    token: Yup.string()
-    .required('Token is required'),
-    email: Yup.string()
-    .email("Email is invalid")
-    .required('Email is required'),
-    password: Yup.string()
-    .required("Password is required")
-    .min(4, "Password should be atleast 4 characters")
-    .max(10, "Password should be not be more than 10 characters")
+  name: Yup.string()
+  .required('Name is required'),
+  email: Yup.string()
+  .email("Email is invalid")
+  .required('Email is required'),
+  phoneNo: Yup.string()
+  .required('Phone number is required'),
+  password: Yup.string()
+  .required("Password is required")
+  .min(4, "Password should be atleast 4 characters")
+  .max(10, "Password should be not be more than 10 characters")
 });
 
+export const loginSchema = Yup.object().shape({
+  email: Yup.string()
+  .email("Email is invalid")
+  .required('Email is required'),
+  username: Yup.string()
+  .required('Username is required'),
+  password: Yup.string()
+  .required("Password is required")
+  .min(4, "Password should be atleast 4 characters")
+  .max(10, "Password should be not be more than 10 characters")
+});
+
+export const forgotPasswordSchema = Yup.object().shape({
+  email: Yup.string()
+  .email("Email is invalid")
+  .required('Email is required'),
+});
+
+export const resetPasswordSchema = Yup.object().shape({
+  token: Yup.string()
+  .required('Token is required'),
+  email: Yup.string()
+  .email("Email is invalid")
+  .required('Email is required'),
+  password: Yup.string()
+  .required("Password is required")
+  .min(4, "Password should be atleast 4 characters")
+  .max(10, "Password should be not be more than 10 characters")
+});
 
 export const registerFormValues = { name: '', email: '', phoneNo: '', password: '' };
-export const loginFormValues =  { email: '', password: '' };
+export const loginFormValues = { email: '', password: '' };
 export const resetPasswordFormValues = { token: '', email: '',  password: ''};
 export const  forgotPasswordFormValues = { email: '',  };
