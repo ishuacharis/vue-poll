3a5b981320eddaa6675f331e58f20ecc8a977a7d
olawale-o <omoogunolawale@gmail.com>
2021-07-19 18:09:26 +0100

    Modify index.js

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,25 +1,17 @@
 import store from '../store';
 
-
-
-
 export const isLoggedIn = () => {    
-
-    return store.getters['auth/isLoggedIn'] || false;
+  return store.getters['auth/isLoggedIn'] || false;
 };
 
 export const getToken = () => {
-    return store.getters['auth/token'] ;
-}
+  return store.getters['auth/token'] ;
+};
 
+export const housemates = () => {  
+  return JSON.parse(localStorage.getItem('HOUSEMATES')) || null
+};
 
-export const housemates = () => {
-    
-    return  JSON.parse(localStorage.getItem('HOUSEMATES')) || null
-}
-
-export const info = () => {
-    
-    return store.getters['auth/user'] ;
-
-}
+export const info = () => {    
+  return store.getters['auth/user'] ;
+};
